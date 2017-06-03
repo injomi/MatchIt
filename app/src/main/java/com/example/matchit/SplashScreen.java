54b20a40409564ea483206bb5d0834c72c29817d
Joel <15sic058t@sit.singaporetech.edu.sg>
2017-06-03 21:04:40 +0800

    Base Files With Splash Screen

---
+++ app/src/main/java/com/example/matchit/SplashScreen.java
@@ -0,0 +1,49 @@
+package com.example.matchit;
+
+/**
+ * Created by Joel on 03-Jun-17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mProgress = (ProgressBar) findViewById(R.id.progressBar2);
+
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress = 0; progress < 100; progress += 10) {
+            try {
+                Thread.sleep(300);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+                //Timber.e(e.getMessage());
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
