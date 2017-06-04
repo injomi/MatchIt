6fe5049ae7488fc5f987a5f80a6c80f8cf4b879d
josephtyx <joseph.teng_2015@sit.singaporetech.edu.sg>
2017-06-04 18:01:41 +0800

    Added nav bar for events, groups, availability, statistics and setting

---
+++ app/src/main/java/com/example/matchit/Events.java
@@ -0,0 +1,26 @@
+package com.example.matchit;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by josephtyx on 4/6/17.
+ */
+
+public class Events extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.events, container, false);
+
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return myView;
+    }
+}
