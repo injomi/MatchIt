8494a527b44a250c8971cd311dc6fa8254d4327d
Joel <15sic058t@sit.singaporetech.edu.sg>
2017-07-16 23:37:13 +0800

    Org settings xml

--- app/src/main/java/com/example/matchit/helper/RVAdapter.java
+++ app/src/main/java/com/example/matchit/helper/RVAdapter.java
@@ -1,7 +1,6 @@
 package com.example.matchit.helper;
 
 import android.app.Activity;
-import android.os.Build;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -14,13 +13,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.matchit.EventDetails;
-import com.example.matchit.Home;
 import com.example.matchit.HomeScreen;
 import com.example.matchit.R;
 import com.example.matchit.model.Event;
 import com.example.matchit.model.Session;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,15 +37,16 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.EventViewHolder> {
 
         EventViewHolder(View itemView) {
             super(itemView);
-            cv = (CardView)itemView.findViewById(R.id.cv);
-            event_desc = (TextView)itemView.findViewById(R.id.event_desc);
-            event_name = (TextView)itemView.findViewById(R.id.event_name);
-            event_pb = (ProgressBar)itemView.findViewById(R.id.event_pb);
-            event_sd = (TextView)itemView.findViewById(R.id.event_sd);
-            final FragmentManager fragmentManager = ((HomeScreen)itemView.getContext()).getFragmentManager();
+            cv = (CardView) itemView.findViewById(R.id.cv);
+            event_desc = (TextView) itemView.findViewById(R.id.event_desc);
+            event_name = (TextView) itemView.findViewById(R.id.event_name);
+            event_pb = (ProgressBar) itemView.findViewById(R.id.event_pb);
+            event_sd = (TextView) itemView.findViewById(R.id.event_sd);
+            final FragmentManager fragmentManager = ((HomeScreen) itemView.getContext()).getFragmentManager();
             itemView.setOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                    Log.i("Test",event_name.getText().toString() + "(" + eventID + ")" + " clicked");
+                @Override
+                public void onClick(View v) {
+                    Log.i("Test", event_name.getText().toString() + "(" + eventID + ")" + " clicked");
                     Bundle bundle = new Bundle();
                     bundle.putInt(EventDetails.EVENT_ID_KEY, eventID);
                     EventDetails eventDetails = new EventDetails();
@@ -67,7 +64,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.EventViewHolder> {
     List<Event> eventsCopy;
     Activity activity;
 
-    public RVAdapter(List<Event> events, Activity a){
+    public RVAdapter(List<Event> events, Activity a) {
         this.events = events;
         eventsCopy = new ArrayList(events);
         activity = a;
@@ -82,15 +79,15 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.EventViewHolder> {
 
     @Override
     public void onBindViewHolder(EventViewHolder eventViewHolder, int i) {
-        String name,description,startDate;
+        String name, description, startDate;
         Event event = events.get(i);
-        if(event!=null){
+        if (event != null) {
             name = event.getName();
             description = event.getDescription();
             startDate = event.getStartDate();
             int max = 0;
             int count = 0;
-            for(Session s : event.getSessions().values()){
+            for (Session s : event.getSessions().values()) {
                 max += s.getVolunteerMax();
                 count += s.getVolunteerNo();
             }
@@ -119,15 +116,15 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.EventViewHolder> {
 
     public void filter(String text) {
         events.clear();
-        if(text.isEmpty()){
+        if (text.isEmpty()) {
             events.addAll(eventsCopy);
-        } else{
+        } else {
             text = text.toLowerCase();
-            for(Event item: eventsCopy){
-                if(item.getName().toLowerCase().contains(text) || item.getCategory().toLowerCase().contains(text) ||
-                        item.getDescription().toLowerCase().contains(text)||item.getHostName().toLowerCase().contains(text)
-                        ||item.getLocation().toLowerCase().contains(text) || item.getStartDate().toLowerCase().contains(text)
-                        || item.getEndDate().toLowerCase().contains(text)){
+            for (Event item : eventsCopy) {
+                if (item.getName().toLowerCase().contains(text) || item.getCategory().toLowerCase().contains(text) ||
+                        item.getDescription().toLowerCase().contains(text) || item.getHostName().toLowerCase().contains(text)
+                        || item.getLocation().toLowerCase().contains(text) || item.getStartDate().toLowerCase().contains(text)
+                        || item.getEndDate().toLowerCase().contains(text)) {
                     events.add(item);
                 }
             }
@@ -139,8 +136,8 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.EventViewHolder> {
                     RVAdapter.this.notifyDataSetChanged();
                 }
             });
-        }catch(NullPointerException e){
-            Log.i("Test","ignorable error");
+        } catch (NullPointerException e) {
+            Log.i("Test", "ignorable error");
         }
     }
 
